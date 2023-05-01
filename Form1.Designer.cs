d1a6e8e5b3ff982ed829bd7569f8a4a915ab5ebb
Armen Mkhoyan <armenmkhoyan2@outlook.com>
2023-05-01 21:23:26 +0400

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Loto
+{
+    partial class frmGame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmGame));
+            this.gbPlayers = new System.Windows.Forms.GroupBox();
+            this.gbNumbers = new System.Windows.Forms.GroupBox();
+            this.lblCurrNumber = new System.Windows.Forms.Label();
+            this.btnLeaveGame = new System.Windows.Forms.Button();
+            this.btnPlayGame = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbPcImg = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbGame = new System.Windows.Forms.GroupBox();
+            this.dgPcLayer = new System.Windows.Forms.DataGridView();
+            this.dgPlayerLayer = new System.Windows.Forms.DataGridView();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.gbPlayers.SuspendLayout();
+            this.gbNumbers.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.gbPcImg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbGame.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPcLayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPlayerLayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbPlayers
+            // 
+            this.gbPlayers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(187)))), ((int)(((byte)(100)))));
+            this.gbPlayers.Controls.Add(this.lblCount);
+            this.gbPlayers.Controls.Add(this.gbNumbers);
+            this.gbPlayers.Controls.Add(this.btnLeaveGame);
+            this.gbPlayers.Controls.Add(this.btnPlayGame);
+            this.gbPlayers.Controls.Add(this.groupBox1);
+            this.gbPlayers.Controls.Add(this.gbPcImg);
+            this.gbPlayers.Location = new System.Drawing.Point(12, 12);
+            this.gbPlayers.Name = "gbPlayers";
+            this.gbPlayers.Size = new System.Drawing.Size(1380, 161);
+            this.gbPlayers.TabIndex = 0;
+            this.gbPlayers.TabStop = false;
+            // 
+            // gbNumbers
+            // 
+            this.gbNumbers.Controls.Add(this.lblCurrNumber);
+            this.gbNumbers.Enabled = false;
+            this.gbNumbers.Location = new System.Drawing.Point(406, 21);
+            this.gbNumbers.Name = "gbNumbers";
+            this.gbNumbers.Size = new System.Drawing.Size(231, 121);
+            this.gbNumbers.TabIndex = 4;
+            this.gbNumbers.TabStop = false;
+            this.gbNumbers.Text = "Number";
+            // 
+            // lblCurrNumber
+            // 
+            this.lblCurrNumber.AutoSize = true;
+            this.lblCurrNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCurrNumber.ForeColor = System.Drawing.Color.Blue;
+            this.lblCurrNumber.Location = new System.Drawing.Point(53, 18);
+            this.lblCurrNumber.Name = "lblCurrNumber";
+            this.lblCurrNumber.Size = new System.Drawing.Size(0, 91);
+            this.lblCurrNumber.TabIndex = 0;
+            // 
+            // btnLeaveGame
+            // 
+            this.btnLeaveGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(117)))), ((int)(((byte)(71)))));
+            this.btnLeaveGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLeaveGame.Location = new System.Drawing.Point(787, 97);
+            this.btnLeaveGame.Name = "btnLeaveGame";
+            this.btnLeaveGame.Size = new System.Drawing.Size(146, 43);
+            this.btnLeaveGame.TabIndex = 3;
+            this.btnLeaveGame.Text = "Leave";
+            this.btnLeaveGame.UseVisualStyleBackColor = false;
+            this.btnLeaveGame.Click += new System.EventHandler(this.btnLeaveGame_Click);
+            // 
+            // btnPlayGame
+            // 
+            this.btnPlayGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(167)))), ((int)(((byte)(71)))));
+            this.btnPlayGame.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlayGame.Location = new System.Drawing.Point(720, 27);
+            this.btnPlayGame.Name = "btnPlayGame";
+            this.btnPlayGame.Size = new System.Drawing.Size(262, 64);
+            this.btnPlayGame.TabIndex = 2;
+            this.btnPlayGame.Text = "Play";
+            this.btnPlayGame.UseVisualStyleBackColor = false;
+            this.btnPlayGame.Click += new System.EventHandler(this.btnPlayGame_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(1047, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(139, 155);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(8, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(124, 121);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(47, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Player";
+            // 
+            // gbPcImg
+            // 
+            this.gbPcImg.Controls.Add(this.pictureBox1);
+            this.gbPcImg.Controls.Add(this.label1);
+            this.gbPcImg.Location = new System.Drawing.Point(192, 0);
+            this.gbPcImg.Name = "gbPcImg";
+            this.gbPcImg.Size = new System.Drawing.Size(153, 155);
+            this.gbPcImg.TabIndex = 0;
+            this.gbPcImg.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 121);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(40, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Computer";
+            // 
+            // gbGame
+            // 
+            this.gbGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(35)))), ((int)(((byte)(26)))));
+            this.gbGame.Controls.Add(this.dgPcLayer);
+            this.gbGame.Controls.Add(this.dgPlayerLayer);
+            this.gbGame.Location = new System.Drawing.Point(12, 179);
+            this.gbGame.Name = "gbGame";
+            this.gbGame.Size = new System.Drawing.Size(1380, 323);
+            this.gbGame.TabIndex = 1;
+            this.gbGame.TabStop = false;
+            // 
+            // dgPcLayer
+            // 
+            this.dgPcLayer.AllowUserToAddRows = false;
+            this.dgPcLayer.AllowUserToDeleteRows = false;
+            this.dgPcLayer.AllowUserToResizeColumns = false;
+            this.dgPcLayer.AllowUserToResizeRows = false;
+            this.dgPcLayer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgPcLayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPcLayer.ColumnHeadersVisible = false;
+            this.dgPcLayer.Enabled = false;
+            this.dgPcLayer.Location = new System.Drawing.Point(16, 51);
+            this.dgPcLayer.MultiSelect = false;
+            this.dgPcLayer.Name = "dgPcLayer";
+            this.dgPcLayer.ReadOnly = true;
+            this.dgPcLayer.RowHeadersVisible = false;
+            this.dgPcLayer.RowHeadersWidth = 51;
+            this.dgPcLayer.RowTemplate.Height = 24;
+            this.dgPcLayer.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgPcLayer.Size = new System.Drawing.Size(664, 225);
+            this.dgPcLayer.TabIndex = 1;
+            this.dgPcLayer.SelectionChanged += new System.EventHandler(this.dgPcLayer_SelectionChanged);
+            // 
+            // dgPlayerLayer
+            // 
+            this.dgPlayerLayer.AllowUserToAddRows = false;
+            this.dgPlayerLayer.AllowUserToDeleteRows = false;
+            this.dgPlayerLayer.AllowUserToResizeColumns = false;
+            this.dgPlayerLayer.AllowUserToResizeRows = false;
+            this.dgPlayerLayer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgPlayerLayer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPlayerLayer.ColumnHeadersVisible = false;
+            this.dgPlayerLayer.Location = new System.Drawing.Point(697, 51);
+            this.dgPlayerLayer.MultiSelect = false;
+            this.dgPlayerLayer.Name = "dgPlayerLayer";
+            this.dgPlayerLayer.ReadOnly = true;
+            this.dgPlayerLayer.RowHeadersVisible = false;
+            this.dgPlayerLayer.RowHeadersWidth = 51;
+            this.dgPlayerLayer.RowTemplate.Height = 24;
+            this.dgPlayerLayer.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgPlayerLayer.Size = new System.Drawing.Size(664, 225);
+            this.dgPlayerLayer.TabIndex = 0;
+            this.dgPlayerLayer.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgPlayerLayer_CellMouseClick);
+            this.dgPlayerLayer.SelectionChanged += new System.EventHandler(this.dgPlayerLayer_SelectionChanged);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCount.Location = new System.Drawing.Point(675, 118);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(64, 25);
+            this.lblCount.TabIndex = 5;
+            this.lblCount.Text = "label3";
+            // 
+            // frmGame
+            // 
+            this.AcceptButton = this.btnPlayGame;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 518);
+            this.Controls.Add(this.gbGame);
+            this.Controls.Add(this.gbPlayers);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmGame";
+            this.Text = "Loto";
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            this.gbPlayers.ResumeLayout(false);
+            this.gbPlayers.PerformLayout();
+            this.gbNumbers.ResumeLayout(false);
+            this.gbNumbers.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.gbPcImg.ResumeLayout(false);
+            this.gbPcImg.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbGame.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPcLayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPlayerLayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbPlayers;
+        private System.Windows.Forms.GroupBox gbPcImg;
+        private System.Windows.Forms.GroupBox gbGame;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLeaveGame;
+        private System.Windows.Forms.Button btnPlayGame;
+        private System.Windows.Forms.DataGridView dgPcLayer;
+        private System.Windows.Forms.DataGridView dgPlayerLayer;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox gbNumbers;
+        private System.Windows.Forms.Label lblCurrNumber;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Loto
+{
+    public partial class frmGame : Form
+    {
+        public frmGame()
+        {
+            InitializeComponent();
+        }
+        private void frmGame_Load(object sender, EventArgs e)
+        {
+            dgPcLayer.RowCount = dgPlayerLayer.RowCount = 3;
+            dgPcLayer.ColumnCount = dgPlayerLayer.ColumnCount = 9;
+            btnLeaveGame.Enabled = false;
+
+            for (int i = 0; i < dgPcLayer.RowCount; i++)
+            {
+                dgPcLayer.Rows[i].Height = dgPlayerLayer.Rows[i].Height = 60;
+
+            }
+            for (int i = 0; i < dgPcLayer.ColumnCount; i++)
+            {
+                dgPcLayer.Columns[i].Width = dgPlayerLayer.Columns[i].Width = 55;
+                dgPcLayer.Columns[i].DefaultCellStyle.Font =
+                    dgPlayerLayer.Columns[i].DefaultCellStyle.Font =
+                    new Font("MS Reference Sans Serif", 16, FontStyle.Regular);
+
+            }
+            dgPlayerLayer.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgPcLayer.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+        }
+        private Random rnd = new Random();
+        private Thread thread;
+        private void btnPlayGame_Click(object sender, EventArgs e)
+        {
+            btnLeaveGame.Enabled = true;
+            btnPlayGame.Enabled = false;
+            gbNumbers.Enabled = true;
+
+            dgPlayerLayer = generateDg(rnd, dgPlayerLayer);
+            dgPcLayer = generateDg(rnd, dgPcLayer);
+
+            generateArrOfRandomNums();
+
+            thread = new Thread(generateNumber);
+
+            thread.Start();
+        }
+        private void dgPcLayer_SelectionChanged(object sender, EventArgs e)
+        {
+            dgPcLayer.ClearSelection();
+        }
+        private void dgPlayerLayer_SelectionChanged(object sender, EventArgs e)
+        {
+            dgPlayerLayer.ClearSelection();
+        }
+        private DataGridView generateDg(Random rnd, DataGridView dg)
+        {
+            byte row;
+            HashSet<byte> randomNumbers = new HashSet<byte>();
+            byte number = 0;
+            for (int i = 0; i < dg.ColumnCount; i++)
+            {
+                row = (byte)rnd.Next(3, 4);
+                for (int j = 0; j < row; j += rnd.Next(1, 3))
+                {
+                    if (i == 0)
+                    {
+                        number = (byte)rnd.Next(1, 10);
+                        if (!randomNumbers.Contains(number))
+                        {
+                            randomNumbers.Add(number);
+                            dg.Rows[j].Cells[i].Value = number;
+                        }
+                    }
+                    else
+                    {
+                        number = (byte)rnd.Next(i * 10, i * 10 + 10);
+                        if (!randomNumbers.Contains(number))
+                        {
+                            randomNumbers.Add(number);
+                            dg.Rows[j].Cells[i].Value = number;
+                        }
+                    }
+                }
+            }
+            return dg;
+        }
+        private void btnLeaveGame_Click(object sender, EventArgs e)
+        {
+            leave();
+        }
+        private void leave()
+        {
+            playersCount = 0;
+            pcsCount = 0;
+
+            lblCurrNumber.Invoke((MethodInvoker)delegate
+            {
+                lblCurrNumber.Text = "";
+            });
+
+            btnLeaveGame.Invoke((MethodInvoker)delegate
+            {
+                btnLeaveGame.Enabled = false;
+            });
+
+            btnPlayGame.Invoke((MethodInvoker)delegate
+            {
+                btnPlayGame.Enabled = true;
+            });
+
+            gbNumbers.Invoke((MethodInvoker)delegate
+            {
+                gbNumbers.Enabled = false;
+            });
+
+            clearDg(dgPcLayer);
+            clearDg(dgPlayerLayer);
+
+            thread.Abort();
+        }
+        private DataGridView clearDg(DataGridView dg)
+        {
+            for (int i = 0; i < dg.RowCount; i++)
+            {
+                for (int j = 0; j < dg.ColumnCount; j++)
+                {
+                    dg.Rows[i].Cells[j].Value = null;
+                    dg.Rows[i].Cells[j].Style.BackColor = Color.White;
+                }
+            }
+            return dg;
+        }
+        private byte playersCount = 0;
+        private byte pcsCount = 0;
+        private void dgPlayerLayer_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (number == (byte)dgPlayerLayer.Rows[e.RowIndex].Cells[e.ColumnIndex].Value)
+                {
+                    dgPlayerLayer.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Blue;
+                    playersCount++;
+                }
+            }
+        }
+        private byte number;
+        private List<byte> numbers;
+        private void generateArrOfRandomNums()
+        {
+            numbers = new List<byte>();
+            Random rnd = new Random();
+            byte k = 0;
+            while (k < 90)
+            {
+                number = (byte)rnd.Next(1, 91);
+                if (!numbers.Contains(number))
+                {
+                    numbers.Add(number);
+                    k++;
+                }
+            }
+        }
+        private void generateNumber()
+        {
+            byte pcNumbersCount = countOfDgNumbers(dgPcLayer);
+            byte playerNumbersCount = countOfDgNumbers(dgPlayerLayer);
+            byte k = 0;
+            while (true)
+            {
+                foreach (var item in numbers)
+                {
+                    number = item;
+                    lblCurrNumber.Invoke((MethodInvoker)delegate
+                    {
+                        lblCurrNumber.Text = item.ToString();
+                    });
+                    checkBelateds(k);
+                    checkPcNumber();
+                    if (playersCount == playerNumbersCount)
+                    {
+                        MessageBox.Show("Player Wins!", "Winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        leave();
+                    }
+                    else if (pcsCount == pcNumbersCount)
+                    {
+                        MessageBox.Show("PC Wins!", "Winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        leave();
+                    }
+                    k++;
+                    Thread.Sleep(2200);
+                }
+            }
+        }
+        private void checkPcNumber() // yntaciq stugum e PC-i tveric voreve meky havasar e generacvacin?
+        {
+            for (int i = 0; i < dgPcLayer.RowCount; i++)
+            {
+                for (int j = 0; j < dgPcLayer.ColumnCount; j++)
+                {
+                    if (dgPcLayer.Rows[i].Cells[j].Value != null && (byte)dgPcLayer.Rows[i].Cells[j].Value == number)
+                    {
+                        dgPcLayer.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                        pcsCount++;
+                        return;
+                    }
+                }
+            }
+        }
+        private byte countOfDgNumbers(DataGridView dg) // hashvum e dg-um exac tveri qanaky
+        {
+            byte count = 0;
+            for (int i = 0; i < dg.RowCount; i++)
+            {
+                for (int j = 0; j < dg.ColumnCount; j++)
+                {
+                    if (dg.Rows[i].Cells[j].Value != null)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+        private void checkBelateds(byte k)//xaxacoxi ushacrac tveri fixum, 1 ushacumic partutyun
+        {
+            for (int i = 0; i < dgPlayerLayer.RowCount; i++)
+            {
+                for (int j = 0; j < dgPlayerLayer.ColumnCount; j++)
+                {
+                    if (k > 0 && dgPlayerLayer.Rows[i].Cells[j].Value != null && dgPlayerLayer.Rows[i].Cells[j].Style.BackColor != Color.Blue && dgPlayerLayer.Rows[i].Cells[j].Style.BackColor != Color.Red && (byte)dgPlayerLayer.Rows[i].Cells[j].Value == numbers[k - 1])
+                    #region comment 
+                        // ete generacvac tivy ka, bayc chi nshvel` texadrvum e karmir X (stuguvum e ete kapuyt back chuni, ete karmir back chuni, ete null che, ev k>0, vor -1-i depqum sxal chta) 
+                    #endregion
+                    {
+                        dgPlayerLayer.Rows[i].Cells[j].Value = "X";
+                        dgPlayerLayer.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                        MessageBox.Show("PC Wins!", "Winner", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        leave();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
